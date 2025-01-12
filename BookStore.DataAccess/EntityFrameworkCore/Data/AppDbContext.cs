ddc4772a7a14c0bbb6edf3d164b237646ae1ac80
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2025-01-12 23:47:42 +0300

    Use Entity Framework and configure Microsoft Identity

---
+++ BookStore.DataAccess/EntityFrameworkCore/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Models.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.DataAccess.EntityFrameworkCore.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
+    {
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
--- BookStore.Web/Program.cs
+++ BookStore.Web/Program.cs
@@ -1,7 +1,21 @@
+using BookStore.DataAccess.EntityFrameworkCore.Data;
+using BookStore.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
+	.AddEntityFrameworkStores<AppDbContext>()
+	.AddDefaultTokenProviders();
 
 var app = builder.Build();
 
