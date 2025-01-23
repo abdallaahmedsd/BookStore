294cdc880f7da2dbb7eca8383c335dec953b5491
Mery Kassis <MeryKassis48@gmail.com>
2025-01-23 16:25:39 +0300

    Edit the the default connection string.

--- BookStore.Web/Program.cs
+++ BookStore.Web/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddRazorPages();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-	options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionMery"));
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
