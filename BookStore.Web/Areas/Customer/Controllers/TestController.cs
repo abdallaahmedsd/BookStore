d35aaa77a4de6f485fc40411e13dcb0d754dd8cf
Mery Kassis <MeryKassis48@gmail.com>
2025-01-31 20:23:07 +0300

    Order project's workflow procces files .

---
+++ BookStore.Web/Areas/Customer/Controllers/TestController.cs
@@ -0,0 +1,10 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Web.Areas.Customer.Controllers
+{
+    public class TestController : Controller
+    {
+        
+    }
+}
