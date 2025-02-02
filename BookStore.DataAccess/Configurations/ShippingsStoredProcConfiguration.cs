64f0b869ac8b10ace746d939f8ddabdb2711a1e1
MohammedFares <xmohammedfares@gmail.com>
2025-02-02 19:37:43 +0300

    Added Shipping Config with repo and entity

--- BookStore.DataAccess/Configurations/ShippingsStoredProcConfiguration.cs
+++ BookStore.DataAccess/Configurations/ShippingsStoredProcConfiguration.cs
@@ -21,40 +21,72 @@ namespace BookStore.DataAccess.Configurations
         /// </summary>
         public static ShippingsStoredProcConfiguration Instance => _instance;
 
+        // Get By IDs stored procedures //
+
         /// <summary>
         /// Gets the stored procedure name for retrieving a Shipping by ID.
         /// </summary>
-        public string GetByIdProcedure => GetStoredProcedureWithSchema("SP_GetShippingById", Schemas.Sales);
+        public string GetByIdProcedure => throw new NotImplementedException();
+
+        /// <summary>
+        /// Gets the stored procedure name for retrieving a Shipping by Order ID.
+        /// </summary>
+        public string GetShippingByOrderID => GetStoredProcedureWithSchema("SP_GetShippingByOrderID", Schemas.Sales);
+
+        /// <summary>
+        /// Gets the stored procedure name for retrieving Orders by User ID.
+        /// </summary>
+        public string GetOrdersByUserID => GetStoredProcedureWithSchema("SP_GetOrdersByUserID", Schemas.Sales);
+
+        // Get All stored procedures //
 
         /// <summary>
         /// Gets the stored procedure name for retrieving all Shippings.
         /// </summary>
-        public string GetAllProcedure => GetStoredProcedureWithSchema("SP_GetAllShippings", Schemas.Sales);
+        public string GetAllProcedure => throw new NotImplementedException();
+
+
+        // Insert stored procedures //
 
         /// <summary>
         /// Gets the stored procedure name for inserting a new Shipping.
         /// </summary>
         public string InsertProcedure => GetStoredProcedureWithSchema("SP_CreateShipping", Schemas.Sales);
 
+
+        // Update stored procedures //
+
         /// <summary>
         /// Gets the stored procedure name for updating a Shipping.
         /// </summary>
-        public string UpdateProcedure => GetStoredProcedureWithSchema("SP_UpdateShipping", Schemas.Sales);
+        public string UpdateProcedure => throw new NotImplementedException();
+
+        /// <summary>
+        /// Gets the stored procedure name for updating Shipping status.
+        /// </summary>
+        public string UpdateShippingStatus => GetStoredProcedureWithSchema("SP_UpdateShippingStatus", Schemas.Sales);
+
+        // Delete stored procedures //
 
         /// <summary>
         /// Gets the stored procedure name for deleting a Shipping.
         /// </summary>
-        public string DeleteProcedure => GetStoredProcedureWithSchema("SP_DeleteShipping", Schemas.Sales);
+        public string DeleteProcedure => throw new NotImplementedException();
+
+        // Check if exists stored procedures //
 
         /// <summary>
         /// Gets the stored procedure name for checking if a Shipping exists by ID.
         /// </summary>
-        public string IsExistsByIdProcedure => GetStoredProcedureWithSchema("Fun_IsShippingExistsById", Schemas.Sales);
+        public string IsExistsByIdProcedure => throw new NotImplementedException();
+
+
+        // Parameter Names //
 
         /// <summary>
         /// Gets the name of the ID parameter.
         /// </summary>
-        public string IdParameterName => "Id";
+        public string IdParameterName => "ShippingID";
 
         /// <summary>
         /// Gets the name of the output parameter for the new ID.
@@ -91,8 +123,7 @@ namespace BookStore.DataAccess.Configurations
                 new SqlParameter("@OrderID", SqlDbType.Int) { Value = entity.OrderID },
                 new SqlParameter("@ShippingAddress", SqlDbType.NVarChar, 500) { Value = entity.ShippingAddress },
                 new SqlParameter("@TrackingNumber", SqlDbType.NVarChar, 100) { Value = entity.TrackingNumber },
-                new SqlParameter("@EstimatedDelivery", SqlDbType.DateTime) { Value = entity.EstimatedDelivery },
-                new SqlParameter("@Status", SqlDbType.TinyInt) { Value = entity.Status }
+                new SqlParameter("@EstimatedDelivery", SqlDbType.DateTime) { Value = entity.EstimatedDelivery }
             };
             command.Parameters.AddRange(paramsShipping);
             SqlParameter idParameter = command.Parameters.Add($"@{Id_Output_ParameterName}", SqlDbType.Int);
