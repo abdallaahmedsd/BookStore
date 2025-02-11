d73e440e500879213b090e3be27a5c60e8333e4d
Mery Kassis <MeryKassis48@gmail.com>
2025-02-11 18:11:38 +0300

    added OrderItemViewModel

---
+++ BookStore.Models/ViewModels/Admin/Order/OrderListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BookStore.Models.Entities;
+using BookStore.Models.ViewModels.Customer.OrderVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.ViewModels.Admin.Order
+{
+    public class OrderListViewModel
+    {
+
+        List<OrderItemViewModel> OrderItems { get; set; }
+
+    }
+}
