25a8598bcd9ce45295cbcb900f720e46a2a3b1ba
Mery Kassis <MeryKassis48@gmail.com>
2025-02-14 14:25:12 +0300

    SetStatus styling functionalirt.

---
+++ BookStore.Web/wwwroot/js/Admin/set-status.js
@@ -0,0 +1,24 @@
+﻿
+const StatusCells = document.querySelectorAll(".status");
+
+StatusCells.forEach(cell => {
+    const status = cell.dataset.status;
+    switch (status) {
+        case SessionHelper.StatusApproved:
+            cell.classList.add("bg-second");
+            break;
+        case SessionHelper.StatusInProcess:
+            cell.classList.add("bg-main");
+            break;
+        case SessionHelper.StatusShipped:
+            cell.classList.add("bg-text");
+            break;
+        case SessionHelper.StatusCanceled:
+            cell.classList.add("bg-red");
+            break;
+        default:
+            cell.classList.add("bg-gray");
+
+    }
+})
+
---
+++ BookStore.Web/wwwroot/js/Customer/set-status.js
@@ -0,0 +1,24 @@
+﻿const StatusCells = document.querySelectorAll(".status");
+
+StatusCells.forEach(cell => {
+    const status = cell.dataset.status;
+
+    switch (status) {
+        case SessionHelper.StatusApproved:
+            cell.classList.add("color-second");
+            break;
+        case SessionHelper.StatusInProcess:
+            cell.classList.add("color-gray");
+            break;
+        case SessionHelper.StatusShipped:
+            cell.classList.add("color-main");
+            break;
+        case SessionHelper.StatusCanceled:
+            cell.classList.add("color-red");
+            break;
+        default:
+            cell.classList.add("bg-gray");
+
+    }
+})
+
